4cf38b468fea2d8ba4abe7ebb9f868c010f5a0e5
River.cao <lijun@10.1.13.100>
2024-05-14 18:43:02 +0800

    creat: add object

---
+++ entry/src/main/ets/mock/ListData.js
@@ -0,0 +1,8 @@
+export let ToDoData = [
+    { name: "早起晨练" },
+    { name: "准备早餐" },
+    { name: "阅读名著" },
+    { name: "学习ArkTS" },
+    { name: "看剧放松" }
+];
+//# sourceMappingURL=ListData.js.map
